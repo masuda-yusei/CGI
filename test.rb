10f81cd5c2540d7e853881abed25b95dd6e44578
yusei masuda <masuda.no@icloud.com>
2021-10-04 23:00:04 +0900

    修正

--- test.rb
+++ test.rb
@@ -7,8 +7,13 @@ server = WEBrick::HTTPServer.new({
 ['INT', 'TERM'].each {|signal|
   Signal.trap(signal){ server.shutdown }
 }
-server.mount('/test', WEBrick::HTTPServlet::ERBHandler, 'test.html.erb')
-server.mount('/indicate.cgi', WEBrick::HTTPServlet::CGIHandler, 'indicate.rb')
-server.mount('/goya.cgi', WEBrick::HTTPServlet::CGIHandler, 'goya.rb')
-server.mount('/kadai', WEBrick::HTTPServlet::ERBHandler, 'kadai.html.erb')
+# server.mount('/test', WEBrick::HTTPServlet::ERBHandler, 'test.html.erb')
+# server.mount('/indicate.cgi', WEBrick::HTTPServlet::CGIHandler, 'indicate.rb')
+# server.mount('/goya.cgi', WEBrick::HTTPServlet::CGIHandler, 'goya.rb')
+server.mount('/', WEBrick::HTTPServlet::ERBHandler, 'kadai_fix.html.erb')
+server.mount('/kadai_goya.cgi', WEBrick::HTTPServlet::CGIHandler, 'kadai_goya.rb')
+server.mount('/kadai_goya_quality.cgi', WEBrick::HTTPServlet::CGIHandler, 'kadai_goya_quality.rb')
 server.start
+
+
+
